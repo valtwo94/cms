cd5ebcb2023fc46dc2399b6ead84d051c149dc53
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-10 00:38:14 +0900

    search 기능 작동확인

--- includes/sidebar_search.php
+++ includes/sidebar_search.php
@@ -1,7 +1,19 @@
 <?php
 if (isset($_POST['search'])) {
-    $search = $_POST['search'];
+    $search = trim($_POST['search']);
+    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+    $search_query = mysqli_query($connection, $query);
+    if (!$search_query) {
+        die('Query FAILED' . mysqli_error($connection));
+    }
 }
+$count = mysqli_num_rows($search_query);
+if ($count == 0) {
+    echo "<h1>no results</h1>";
+} else {
+    echo $count;
+}
+
 
 ?>
 
