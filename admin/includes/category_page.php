18e31c08d7de335994cbc8fad63cfa7a6527d7da
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-10 15:19:39 +0900

    카테고리 목록 보여줄 테이블 추가

--- admin/includes/category_page.php
+++ admin/includes/category_page.php
@@ -8,15 +8,37 @@
                 <h1 class="page-header">
                     카테고리
                 </h1>
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="cat-title">카테고리 추가</label>
-                        <input class="form-control" type="text" name="cat_title">
-                    </div>
-                    <div class="form-group">
-                        <input class="btn btn-primary" type="submit" name="submit" value="카테고리 추가">
-                    </div>
-                </form>
+                <div class="col-xs-6">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="cat-title">카테고리 추가</label>
+                            <input class="form-control" type="text" name="cat_title">
+                        </div>
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="submit" value="카테고리 추가">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-xs-6">
+                    <table class="table table-border">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Category title</th>
+                            </tr>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>basketball</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>baseball</td>
+                            </tr>
+                        </tbody>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div> <!-- /.row -->
 
