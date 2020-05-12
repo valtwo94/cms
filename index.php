6ad17cc2a6ebd650bfdded346280994c53db44e4
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-12 15:09:40 +0900

    각 포스트 부르는 함수 컴포넌트화

--- index.php
+++ index.php
@@ -5,18 +5,12 @@
 
 <!-- Page Content -->
 <div class="container">
-
     <div class="row">
-
         <!-- Blog Entries Column -->
         <div class="col-md-8">
             <?php include './db/all_post.php' ?>
-
         </div>
-
         <!-- Blog Sidebar Widgets Column -->
         <?php include './includes/sidebar.php' ?>
-
     </div>
-
     <?php include './includes/footer.php' ?>
