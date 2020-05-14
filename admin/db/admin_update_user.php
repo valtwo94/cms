f1319577e20dee549e407341f093f9245f3c9e83
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-14 14:17:33 +0900

    사용자 수정기능 작동확인

--- admin/db/admin_update_user.php
+++ admin/db/admin_update_user.php
@@ -1,46 +1,34 @@
 <?php
-$user_id = $_GET['update'];
-if (isset($_GET['update'])) {
 
+if (isset($_GET['update'])) {
+    $user_id = $_GET['update'];
     $query = "SELECT * FROM users WHERE user_id = $user_id";
     $select_user_by_id = mysqli_query($connection, $query);
-
-
     $row = mysqli_fetch_assoc($select_user_by_id);
     $user_email = $row['user_email'];
     $user_password = $row['user_password'];
     $user_name = $row['user_name'];
     $user_image = $row['user_image'];
-    $user_date = $row['user_date'];
 }
 
-if (isset($_POST['update_post'])) {
+if (isset($_POST['update_user'])) {
+    $user_id = $_GET['update'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    $user_name = $_POST['user_name'];
+    $user_role = $_POST['user_role'];
+    $user_image = $_FILES['user_image']['name'];
+    $user_image_temp = $_FILES['user_image']['tmp_name'];
 
-    $post_title = $_POST['post_title'];
-    $post_author = $_POST['post_author'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
-    $post_image = $_FILES['post_image']['name'];
-    $post_image_temp = $_FILES['post_image']['tmp_name'];
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
-
-
-
-    move_uploaded_file($post_image_temp, "../images/$post_image");
-    $query = "UPDATE posts SET ";
-    $query .= "post_category_id ='{$post_category_id}', ";
-    $query .= "post_title = '{$post_title}', ";
-    $query .= "post_author = '{$post_author}', ";
-    $query .= "post_date = now(), ";
-    $query .= "post_image = '{$post_image}', ";
-    $query .= "post_content = '{$post_content}', ";
-    $query .= "post_tags = '{$post_tags}', ";
-    $query .= "post_status = '{$post_status}' ";
-    $query .= "WHERE post_id = {$post_id}";
-    $add_post_query = mysqli_query($connection, $query);
-
-    confirm($add_post_query);
-    header("Location: posts.php?source=all_post");
+    move_uploaded_file($user_image_temp, "../images/$user_image");
+    $query = "UPDATE users SET ";
+    $query .= "user_email = '{$user_email}', ";
+    $query .= "user_password = '{$user_password}', ";
+    $query .= "user_image = '{$user_image}', ";
+    $query .= "user_role = '{$user_role}', ";
+    $query .= "user_name = '{$user_name}' ";
+    $query .= "WHERE user_id = {$user_id} ";
+    $upadte_user_query = mysqli_query($connection, $query);
+    confirm($upadte_user_query);
+    header("Location: users.php?source=");
 }
--- admin/includes/pages/update_user_page.php
+++ admin/includes/pages/update_user_page.php
@@ -10,21 +10,29 @@
                 <form action="" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="user_email">이메일</label>
-                        <input type="email" class="form-control" name="user_email" value="<?php echo $row['post_title'] ?>">
+                        <input type="email" class="form-control" name="user_email" value="<?php echo $user_email ?>">
                     </div>
                     <div class="form-group">
                         <label for="user_password">비밀번호</label>
-                        <input type="password" class="form-control" name="user_password">
+                        <input type="password" class="form-control" name="user_password" value="user_password">
+                    </div>
+                    <div class="form-group">
+                        <select name="user_role" id="">
+                            <option value="일반">일반</option>
+                            <option value="관리자">관리자</option>
+                        </select>
                     </div>
                     <div class="form-group">
                         <label for="user_name">이름</label>
-                        <input type="text" class="form-control" name="user_name">
+                        <input type="text" class="form-control" name="user_name" value="<?php echo $user_name ?>">
                     </div>
                     <div class="form-group">
                         <label for="post_image">프로필 이미지</label>
-                        <input type="file" name="post_image">
+                        <input type="file" name="user_image">
                         <br>
-                        <img width="100" src="../images/<?php echo $post_image ?>" alt="">
+                        <label>현재 이미지</label>
+                        <br>
+                        <img width="100" src="../images/<?php echo $user_image ?>" alt="">
                     </div>
                     <div class="form-group">
                         <input class="btn btn-primary" type="submit" name="update_user" value="수정">
