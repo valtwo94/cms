03fdb33b5b9438653c1465204f5b42752f8fa80b
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-14 10:05:47 +0900

    admin user 생성 화면 추가, db작동확인

---
+++ admin/db/admin_add_user.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['create_user'])) {
+
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    $user_name = $_POST['user_name'];
+    $user_image = $_FILES['user_image']['name'];
+    $user_image_temp = $_FILES['user_image']['tmp_name'];
+    $user_role = $_POST['user_role'];
+    $user_date = date('d-m-y');
+
+    move_uploaded_file($user_image_temp, "../images/$user_image");
+    $query = "INSERT INTO users(user_email, user_password, user_name, user_date, user_image, user_role ) ";
+    $query .= "VALUES('{$user_email}', '{$user_password}', '{$user_name}', now(), '{$user_image}', '{$user_role}' ) ";
+    $add_user_query = mysqli_query($connection, $query);
+    confirm($add_user_query);
+}
---
+++ admin/includes/pages/add_user_page.php
@@ -0,0 +1,49 @@
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    글 추가
+                </h1>
+                <?php include 'db/admin_add_user.php' ?>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="">이메일</label>
+                        <input type="email" class="form-control" name="user_email">
+                    </div>
+                    <div class="form-group">
+                        <label for="post_category">비밀번호</label>
+                        <input type="password" class="form-control" name="user_password">
+                    </div>
+                    <div class="form-group">
+                        <label for="post_author">사용자 이름</label>
+                        <input type="text" class="form-control" name="user_name">
+                    </div>
+                    <div class="form-group">
+                        <label for="user_role">계정유형</label>
+                        <select name="user_role">
+                            <option value="관리자">관리자</option>
+                            <option value="일반">일반</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="post_image">이미지</label>
+                        <input type="file" name="user_image">
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-primary" type="submit" name="create_user" value="추가">
+                    </div>
+
+                </form>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
