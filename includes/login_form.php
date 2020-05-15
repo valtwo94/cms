fe90d2ed45597bed1ac1fb7d2c34f5c5bd31a8b8
성현제 <hyunjaesung@seonghyeonje.local>
2020-05-15 12:24:47 +0900

    유저 화면 로그아웃 버튼 생성 작동 확인

--- includes/login_form.php
+++ includes/login_form.php
@@ -1,15 +1,25 @@
 <?php include 'db/login.php'; ?>
-<div class="well">
-    <form action="" method="post" class="form-group">
-        <div class="form-group">
-            <input type="email" name=user_email class="form-control" placeholder="이메일">
-        </div>
-        <div class="form-group">
-            <input type="password" name="user_password" class="form-control" placeholder="비밀번호">
+<?php if (!isset($_SESSION['user_name'])) {
+?>
+    <div class="well">
+        <form action="" method="post" class="form-group">
+            <div class="form-group">
+                <input type="email" name=user_email class="form-control" placeholder="이메일">
+            </div>
+            <div class="form-group">
+                <input type="password" name="user_password" class="form-control" placeholder="비밀번호">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="로그인" class="btn btn-primary" name="login">
+            </div>
+        </form>
+    </div>
+<?php
+} else { ?>
 
-        </div>
-        <div class="form-group">
-            <input type="submit" value="로그인" class="btn btn-primary" name="login">
-        </div>
-    </form>
-</div>
+    <div class="well">
+        <a href="db/logout.php" class="btn btn-primary">로그아웃</a>
+    </div>
+<?php
+}
+?>
